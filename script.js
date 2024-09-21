6b67ae61b4c2104ec63a7e08f3b81427f3f9a5bb
sharonodongo <sharon94odongo@gmail.com>
2024-09-22 01:24:02 +0300

    Added content and linked script.js for dynamic content

---
+++ script.js
@@ -0,0 +1,11 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+document.querySelector("body").appendChild(h2);
+// Create an h2 element
+const h2Element = document.createElement('h2');
+
+// Add text content to the h2 element
+h2Element.textContent = 'Hello from JavaScript!';
+
+// Append the h2 element to the body of the document
+document.body.appendChild(h2Element);
